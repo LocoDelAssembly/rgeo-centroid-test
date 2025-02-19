4868dca30c87f412b33f69775f375bb5277f4dc3
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2025-02-19 20:16:22 -0300

    polygon.projection.centroid == expected_centroid test added

--- script.rb
+++ script.rb
@@ -11,8 +11,12 @@ puts "PROJECTED CENTROID: #{polygon.projection.centroid.inspect}"
 puts "UNPROJECTED CENTROID: #{factory.unproject(polygon.projection.centroid).inspect}"
 puts "EXPECTED CENTROID: #{expected_centroid.inspect}"
 puts "ACTUAL CENTROID: #{polygon.centroid.inspect}"
+puts "ACTUAL PROJECTION CENTROID: #{polygon.projection.centroid.inspect}"
 puts "EXPECTED CENTROID PROJECTION: #{expected_centroid.projection.inspect}"
 puts "EXPECTED CENTROID PROJECTION UNPROJECTED: #{factory.unproject(expected_centroid.projection).inspect}"
+
+raise "INVALID CENTROID" unless polygon.projection.centroid == expected_centroid.projection
+puts "polygon.projection.centroid == expected_centroid.projection TEST PASSED"
 
 raise "INVALID CENTROID" unless polygon.centroid == expected_centroid
 puts "polygon.centroid == expected_centroid TEST PASSED"
